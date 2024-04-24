370d2510ebb66341adfd9ebb0d444ab750e3b26e
Luxu <122475744+luxu-gg@users.noreply.github.com>
2024-04-24 16:50:13 +0100

    Fix app store images
    - Added description property
    - Added webUrl property
    
    App store images start from 0.png

--- templates/vanila-js/init.lua
+++ templates/vanila-js/init.lua
@@ -2,23 +2,27 @@
 ---@class okokApp
 ---@field id string @ App ID
 ---@field label string @ App name
+---@field description string @ App description (App Store)
 ---@field custom true @ Is this app custom?
 ---@field icon string @ Icon image file "icon.png"
 ---@field previewImagesCount number @ How many images does this app have (App Store)?
 ---@field notifications boolean @ Does this app have notifications?
 
 local resourceName = GetCurrentResourceName()
+local webUrl       = ("https://cfx-nui-%s/web/"):format(resourceName)
 local logoPath     = ("https://cfx-nui-%s/web/icon.png"):format(resourceName)
 
 ---@type okokApp
 local appInfo      = {
       id = "customapp",
       label = "Custom App Name",
+      description = "This is a custom app for the okokPhone.",
       custom = true,
       icon = logoPath,
       previewImagesCount = 3,
       notifications = true,
-      resourceName = resourceName
+      resourceName = resourceName,
+      webUrl = webUrl,
 }
 --- Initialize the app
 CreateThread(function()
